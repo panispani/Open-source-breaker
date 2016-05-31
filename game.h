2bdda967abd7fa6345f804b2a82cd387285fe497
panpan2 <p.panayiotou2@gmail.com>
2016-05-31 16:42:40 +0100

    Main game loop

--- game.h
+++ game.h
@@ -1,6 +1,16 @@
 #ifndef __GAME_H
 #define __GAME_H
 
+typedef enum {
+    START_GAME, START_MENU, PLAY_GAME, PAUSE_SCREEN, LOSE_GAME, GAME_OVER, WIN_LEVEL, WIN_GAME
+} game_state_t;
+
+typedef struct {
+    int32_t x;
+    int32_t y;
+    int32_t old_x;
+    int32_t old_y;
+} ball_t;
 
 
 
--- main.c
+++ main.c
@@ -1,1 +1,83 @@
+#include "includes.h"
 
+void run(game_state_t game_state);
+void start_game();
+void start_menu();
+void play_game();
+void pause_screen();
+void lose_game();
+void game_over();
+void win_level();
+void win_game();
+
+int main(void) {
+    game_state_t game_state = START_GAME;
+    run(game_state);    
+}
+
+
+void run(game_state_t game_state) {
+    while(1) {
+        switch(game_state) {
+            case START_GAME:
+                start_game();
+                break;
+            case START_MENU:
+                start_menu();
+                break;
+            case PLAY_GAME:
+                play_game();
+                break;
+            case PAUSE_SCREEN:
+                pause_screen();
+                break;
+            case LOSE_GAME:
+                lose_game();
+                break;
+            case GAME_OVER:
+                game_over();
+                break;
+            case WIN_LEVEL:
+                win_level();
+                break;
+            case WIN_GAME:
+                win_game();
+                break;
+            default:
+                perror("Error game state not found");
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void start_game() {
+
+}
+
+void start_menu() {
+
+}
+
+void play_game() {
+
+}
+
+void pause_screen() {
+
+}
+
+void lose_game() {
+
+}
+
+void game_over() {
+
+}
+
+void win_level() {
+
+}
+
+void win_game() {
+
+}
