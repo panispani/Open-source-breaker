3f168c702e00b8c6235171f09f753a0c29385cd9
panpan2 <p.panayiotou2@gmail.com>
2016-05-31 15:49:56 +0100

    Common includes file

---
+++ includes.h
@@ -0,0 +1,22 @@
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
+/*
+ * C standard library
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+
+/*
+ * Header files
+ */
+#include "controller.h"
+#include "graphics.h"
+#include "game.h"
+
+#endif //__INCLUDES_H
