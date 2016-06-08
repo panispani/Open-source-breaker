374cf44f5ae8674b995a5ba67999fb6711f67812
Peter Kovary <peter.kovary15@imperial.ac.uk>
2016-06-08 20:26:26 +0100

    [Includes] Change to universal format

--- includes.h
+++ includes.h
@@ -9,9 +9,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <SDL.h>
-#include <SDL_mixer.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
 
 /*
  * Header files
