3dc7a803638bf7f0d29e6ce8dc8c24bbc7ca2938
panpan2 <p.panayiotou2@gmail.com>
2016-06-08 16:41:27 +0100

    [game] made speeds more human, bar is not smooth yet

--- game.c
+++ game.c
@@ -4,14 +4,14 @@
 #define DEF_BAR_Y (15 * gameheight / 16)
 #define DEF_BAR_X gamewidth / 2 + 20
 #define DEF_BAR_HEIGHT 6
-#define DEF_BALL_RADIUS 10
+#define DEF_BALL_RADIUS 4
 #define DEF_BALL_HEIGHT (2 * gameheight / 3)
 #define DEF_BALL_DX 3
 #define DEF_BALL_DY 8
-#define BAR_MAX_SPEED 20
+#define BAR_MAX_SPEED 25
 #define BAR_SPEED_UP 20
 #define BAR_BOUNCE 0.5
-#define BALL_MAX_SPEED 14
+#define BALL_MAX_SPEED 10
 #define BRICK_X_OFFSET (gamewidth - BRICKS_PER_ROW * BRICK_WIDTH) / 2
 #define BRICK_Y_OFFSET gameheight / 14
 #define LOSE_Y_LIMIT bar->height
